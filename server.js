75d4993f140326b89cad8d8d5b0b8f920f53ca8c
Ubuntu <isans@isans.eijnf4u0ygce3fsjkpfpevhjrc.hx.internal.cloudapp.net>
2015-12-15 10:11:05 +0000

    change...

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ var http = require('http')
 var port = process.env.PORT || 13371;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+  res.end('Hello Azuer\n');
+}).listen(port, function(){
+	console.log(port);
+});
